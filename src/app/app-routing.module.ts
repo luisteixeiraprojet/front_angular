65fe799346e57d0870b8dc5b0c3be2d8f9f47920
luisteixeiraprojet <luisteixeiraprojet@gmail.com>
2021-01-12 18:16:25 +0100

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AllEmployeesComponent } from './all-employees/all-employees.component';
+import { EmployeurComponent } from './employeur/employeur.component';
+
+const routes: Routes = [
+  {path:'', component: EmployeurComponent},
+  {path:'/allEmployees', component: AllEmployeesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeurComponent } from './employeur/employeur.component';
+import { AllEmployeesComponent } from './all-employees/all-employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmployeurComponent,
+    AllEmployeesComponent
   ],
   imports: [
     BrowserModule,
